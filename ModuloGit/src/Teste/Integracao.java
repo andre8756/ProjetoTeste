b96c2e5942c90049998600c25defed066772bdea
andre8756 <153465812+andre8756@users.noreply.github.com>
2024-11-25 21:26:37 -0300

    terceira alteraca

--- ModuloGit/src/Teste/Integracao.java
+++ ModuloGit/src/Teste/Integracao.java
@@ -3,5 +3,6 @@ package Teste;
 public class Integracao {
 	public static void main(String[] args){
 		System.out.println("testando commit");
+		System.out.println("Testando commit do github")
 	}
 }
